143c41c8fc0967781a1eafd3b67440b4ac42e4ef
elin <elin@playwings.co.kr>
2018-10-22 14:29:33 +0900

    API 연동해 image 또는 media 보여주기

--- src/App.js
+++ src/App.js
@@ -2,16 +2,66 @@ import React, { Component } from 'react';
 import ViewerTemplate from './components/ViewerTemplate';
 import SpaceNavigator from './components/SpaceNavagator';
 import Viewer from './components/Viewer';
+import * as api from './lib/api';
 
 class App extends Component {
+  state = {
+    loading: false,
+    maxDate: null,
+    date: null,
+    url: null,
+    mediaType: null
+  }
+
+  getAPOD = async (data) => {
+    if(this.state.loading) return; // 이미 요청중이라면 무시
+
+    // 로딩 상태 시작
+    this.setState({
+      loading: true
+    });
+
+    try {
+      const response = await api.getAPOD(data);
+      const {date: retrievedDate, url, media_type: mediaType} = response.data;
+
+      if(!this.state.maxDate) {
+        this.setState({
+          maxDate: retrievedDate
+        })
+      }
+
+      this.setState({
+        date: retrievedDate,
+        mediaType,
+        url
+      });
+      console.log(response);
+    } catch(e) {
+      console.log(e);
+    }
+
+    // 로딩 상태 종료
+    this.setState({
+      loading: false
+    });
+  }
+
+  componentDidMount() {
+    this.getAPOD();
+  }
+
   render() {
+    const {url, mediaType, loading} = this.state;
+
     return (
       <ViewerTemplate
         spaceNavigator={<SpaceNavigator/>}
         viewer={(
           <Viewer
-            url="https://www.youtube.com/embed/uj3Lq7Gu94Y?rel=0"
-            mediaType="video"
+            url={url}
+            mediaType={mediaType}
+            loading={loading}
           />
         )}
       />
