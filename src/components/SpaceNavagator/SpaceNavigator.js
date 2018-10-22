9c5850b12ff2e575e804ddc95a7d0a4b66768c9b
elin <elin@playwings.co.kr>
2018-10-22 11:17:45 +0900

    SpaceNavigator component

---
+++ src/components/SpaceNavagator/SpaceNavigator.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './SpaceNavigator.scss';
+import classNames from 'classnames/bind';
+import {MdChevronLeft as LeftIcon, MdChevronRight as RightIcon} from 'react-icons/md';
+
+const cx = classNames.bind(styles);
+
+const SpaceNavigator = ({onPrev, onNext}) => (
+    <div className={cx('space-navigator')}>
+      <div className={cx('left', 'end')}>
+        <div className={cx('circle')} onClick={onPrev}>
+          <LeftIcon/>
+        </div>
+      </div>
+      <div className={cx('right', 'end')}>
+        <div className={cx('circle')} onClick={onNext}>
+          <RightIcon/>
+        </div>
+      </div>
+    </div>
+);
+
+export default SpaceNavigator;
